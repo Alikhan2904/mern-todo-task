be8555d9e2ed16379b0042a354528163776873d6
Alikhan2904 <alikhan.sachwani@gmail.com>
2020-08-07 18:41:38 +0500

    Login and Register backend done

---
+++ routes/Users.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const users = express.Router();
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
+const User = require('../models/User');
+users.use(cors());
+
+process.env.SECRET_KEY = 'abc.123';
+
+users.post('/register', (req, res) => {
+  const today = new Date();
+  const userData = {
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    email: req.body.email,
+    password: req.body.password,
+    created: today,
+  };
+
+  User.findOne({
+    email: req.body.email,
+  })
+    .then((user) => {
+      // if there is no existing user with that email
+      if (!user) {
+        bcrypt.hash(req.body.password, 10, (err, hash) => {
+          userData.password = hash;
+          User.create(userData)
+            .then((user) => {
+              res.json({ status: user.email + ' Registered!' });
+            })
+            .catch((err) => {
+              res.send('error: ' + err);
+            });
+        });
+      } else {
+        // if there is an existing user with that email
+        res.json({ error: 'User already exists' });
+      }
+    })
+    .catch((err) => {
+      res.send('error: ' + err);
+    });
+});
+
+users.post('/login', (req, res) => {
+  User.findOne({
+    email: req.body.email,
+  })
+    .then((user) => {
+      if (user) {
+        if (bcrypt.compareSync(req.body.password, user.password)) {
+          // Passwords match
+          const payload = {
+            _id: user._id,
+            first_name: user.first_name,
+            last_name: user.last_name,
+            email: user.email,
+          };
+          let token = jwt.sign(payload, process.env.SECRET_KEY, {
+            expiresIn: 1440,
+          });
+          res.send(token);
+        } else {
+          // Passwords don't match
+          res.json({ error: 'Incorrect Password' });
+        }
+      } else {
+        // User does not exist
+        res.json({ error: 'User does not exist' });
+      }
+    })
+    .catch((err) => {
+      res.send('error: ' + err);
+    });
+});
+
+module.exports = users;
---
+++ server.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var cors = require('cors');
+var bodyParser = require('body-parser');
+
+var app = express();
+var mongoose = require('mongoose');
+
+var port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(
+    bodyParser.urlencoded({
+        extended: false
+    })
+);
+
+const mongoURI = 'mongodb+srv://Alikhan:Alikhan29@@mycluster.uhmwd.mongodb.net/ignite-labs-todo?retryWrites=true&w=majority'
+
+mongoose
+    .connect(mongoURI, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
+
+var Users = require('./routes/Users')
+
+app.use('/users', Users);
+
+
+app.listen(port, () => {
+    console.log('Server running on port: ' + port);
+});
+
+
