b6e8b253d1e99a1306fa4f6f0ec0a45825792d43
Ali Khan <ali.khan@sadapay.pk>
2023-07-31 02:35:07 +0500

    deploy build folder

---
+++ client/build/precache-manifest.c2417545a664600dafa07f216cd777a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78f9d062d9be4033316ff8352cd01359",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ecf22bda5850baf08efc",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ba77ec3c058590c4b0ee",
+    "url": "/static/js/2.6613de19.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6613de19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecf22bda5850baf08efc",
+    "url": "/static/js/main.b7098401.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
